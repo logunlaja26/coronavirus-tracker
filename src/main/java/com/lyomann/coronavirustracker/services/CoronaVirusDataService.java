6e52e7751f617f966134521dc655c01a34b3b77d
Ly Ogunlaja <logunlaja26@gmail.com>
2020-04-05 09:41:42 -0500

    updated csv file link

--- src/main/java/com/lyomann/coronavirustracker/services/CoronaVirusDataService.java
+++ src/main/java/com/lyomann/coronavirustracker/services/CoronaVirusDataService.java
@@ -19,7 +19,7 @@ import java.util.List;
 @Service
 public class CoronaVirusDataService {
 
-    private static final String VIRUS_DATA_URL = "https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_19-covid-Confirmed.csv";
+    private static final String VIRUS_DATA_URL = "https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_covid19_confirmed_global.csv";
     private static final Logger log = LoggerFactory.getLogger(CoronaVirusDataService.class);
 
     private List<LocationStats> allStats = new ArrayList<>();
